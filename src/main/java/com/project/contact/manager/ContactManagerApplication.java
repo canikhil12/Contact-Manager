2b7d078476ab8b5a7a62c3c78178765f6309e385
canikhil12 <75828601+canikhil12@users.noreply.github.com>
2024-06-05 10:22:09 -0400

    contact

---
+++ src/main/java/com/project/contact/manager/ContactManagerApplication.java
@@ -0,0 +1,17 @@
+package com.project.contact.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagerApplication.class, args);
+	}
+
+}
+
+
+
+
---
+++ src/main/java/com/project/contact/manager/entity/Contact.java
@@ -0,0 +1,5 @@
+package com.project.contact.manager.entity;
+
+public class Contact {
+
+}
